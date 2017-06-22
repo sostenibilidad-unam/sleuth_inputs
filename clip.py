232f8fb45bceee789bc31aaeee8870a16ed94b60
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-06-22 17:25:12 -0500

    will write a gif file

--- clip.py
+++ clip.py
@@ -1,16 +1,20 @@
-from sh import gdalwarp
+from sh import gdalwarp, gdal_translate
 
 
-def clip(shape, tiff, location, output):
+def clip(shape, tiff, location, gif_output):
+    tif_output = gif_output[:-3] + "tif"
     gdalwarp("-cutline", shape,
              "-crop_to_cutline", 
              "-cwhere", 'location="%s"' % location,
              "-of", 'GTiff',
              tiff,
-             output)
+             tif_output)
 
+    gdal_translate("-of", "GIF",
+                   tif_output,
+                   gif_output)
 
 # clip('General/delegaciones.shp',
-#      'General/General.urban.1980.tif',
-#      'Iztapalapa',
-#      'iz.tif')
+#       'General/General.urban.1980.tif',
+#       'Iztapalapa',
+#       'iz.gif')
